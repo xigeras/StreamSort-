22dbf7e293d4fe8b852a905672ba402a17742724
august b. <69170271+xigeras@users.noreply.github.com>
2025-08-24 14:52:19 +0000

    made an exception

--- queue_manager.h
+++ queue_manager.h
@@ -13,6 +13,13 @@ struct Show {
     double rating; // between 1.0 and 5.0
 };
 
+// exception for shows not found
+class ShowNotFoundException : public exception {
+    virtual const char* what() const throw() {
+        return "Show not found in the queue.";
+    }
+};
+
 void addShow(vector<Show> &queue);
 void removeShow(vector<Show> &queue);
 void displayRecursiveQueue(const vector<Show> &queue, size index = 0);
