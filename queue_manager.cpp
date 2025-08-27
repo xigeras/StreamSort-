ff8cd3305f687720689bc7464a8b809e4cb991b5
august b. <69170271+xigeras@users.noreply.github.com>
2025-08-27 23:44:24 +0000

    e

--- queue_manager.cpp
+++ queue_manager.cpp
@@ -1,6 +1,8 @@
 #include "queue_manager.h"
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <stdexcept>
 
 
 void addShow(vector<Show> &queue) {
@@ -29,3 +31,32 @@ void addShow(vector<Show> &queue) {
         cout << "Error: " << e.what() << "\nShow not added.\n";
     }
 }
+
+void removeShow(vector<Show> &queue) {
+    cout << "Enter title of the show to remove: ";
+    string title;
+    getline(cin, title);
+    auto it = find_if(queue.begin(), queue.end(), [&title](const Show& s) {
+        return s.title == title;
+    });
+    if (it != queue.end()) {
+        queue.erase(it);
+        cout << "Show removed!\n";
+    } else {
+        try {
+            throw ShowNotFoundException(title);
+        } catch (ShowNotFoundException& e) {
+                cout << "Error: " << e.what() << "\n";
+        }
+    }
+}
+
+void displayRecursiveQueue(const vector<Show> &queue, size_t index = 0) {
+    
+}
+
+
+void sortQueue(vector<Show>& queue);
+void searchShow(const vector<Show>& queue);
+void saveQueue(const vector<Show>& queue, const string& filename);
+void loadQueue(vector<Show>& queue, const string& filename);
